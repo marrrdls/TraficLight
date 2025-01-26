60c09983c2b21736187032fd6a4874e4f61ce847
marrrdls <marrrdls.com@gmail.com>
2025-01-26 18:28:18 +0100

    Initial Commit

---
+++ Task 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task 2
+//
+//  Created by Margarita on 26.1.25..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
