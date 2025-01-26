792733c03900f6dad2c2cf766b2e17c34c0d0cf5
marrrdls <marrrdls.com@gmail.com>
2025-01-26 19:16:35 +0100

    trafic light

--- Task 2/ViewController.swift
+++ Task 2/ViewController.swift
@@ -7,10 +7,54 @@
 
 import UIKit
 
+enum Lights {
+    case red, yellow, green
+}
 class ViewController: UIViewController {
 
+    var currentLight: Lights = .red
+    
+    @IBOutlet weak var redView: UIView! {
+        didSet {
+            redView.layer.cornerRadius = redView.frame.width / 2
+            
+        }
+    }
+
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
+    @IBAction func next(_ sender: Any) {
+        startButton.setTitle("Next", for: .normal)
+        switch currentLight {
+        case .red:
+            redView.alpha = 0.3
+            yellowView.alpha = 1
+            currentLight = .yellow
+        case .yellow:
+            yellowView.alpha = 0.3
+            greenView.alpha = 1
+            currentLight = .green
+        case .green:
+            greenView.alpha = 0.3
+            redView.alpha = 1
+            currentLight = .red
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        redView.alpha = 0.3
+        yellowView.alpha = 0.3
+        greenView.alpha = 0.3
+        
+        yellowView.layer.cornerRadius = yellowView.frame.width / 2
+        greenView.layer.cornerRadius = greenView.frame.width / 2
+        
+        startButton.layer.cornerRadius = 10
+        startButton.setTitle("Start", for: .normal)
         // Do any additional setup after loading the view.
     }
 
